90b95259150a4b0e7c7cf93c70a053e37f0a7ce0
Abdelrhman Mamdouh <a.mammdouh97@gmail.com>
2025-04-06 00:18:34 +0200

    handle default value

--- employees/models.py
+++ employees/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from departments.models import Department  
-
+from datetime import datetime
 class Employee(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
     phone_number = models.CharField(max_length=15, blank=True, null=True)  # New field added
-    hire_date = models.DateField()
+    hire_date = models.DateField(default=datetime.now())
     job_title = models.CharField(max_length=50)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
 
