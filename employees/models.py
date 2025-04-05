cb28f5c0757fd3abb8c34dbb557b4881fe0797ff
Abdelrhman Mamdouh <a.mammdouh97@gmail.com>
2025-04-05 23:40:05 +0200

    add new fields to employee

--- employees/models.py
+++ employees/models.py
@@ -5,6 +5,8 @@ class Employee(models.Model):
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
     phone_number = models.CharField(max_length=15, blank=True, null=True)  # New field added
+    hire_date = models.DateField()
+    job_title = models.CharField(max_length=50)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
