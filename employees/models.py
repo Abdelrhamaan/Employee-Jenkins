1e286edc6ddc38a487ae8e5a01384b0f53fe3b14
Abdelrhman Mamdouh <a.mammdouh97@gmail.com>
2025-04-06 00:12:56 +0200

    mount volume and add new field to test

--- employees/models.py
+++ employees/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from departments.models import Department  
 
 class Employee(models.Model):
     first_name = models.CharField(max_length=50)
@@ -7,6 +8,7 @@ class Employee(models.Model):
     phone_number = models.CharField(max_length=15, blank=True, null=True)  # New field added
     hire_date = models.DateField()
     job_title = models.CharField(max_length=50)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
